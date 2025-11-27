17d51bf8d54807946615b0f8a351e4515fcb3df6
Chatheriyan T <chatheriyan2308@gmail.com>
2025-11-27 13:04:03 +0530

    Debugging mailer --using gmail api

--- mailer.go
+++ mailer.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"bytes"
+	"context"
+	"encoding/base64"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
-	"net/smtp"
-	"os"
-)
 
-var smtpServer = "smtp.gmail.com"
-var smtpPort = "587"
+	"google.golang.org/api/gmail/v1"
+	"google.golang.org/api/option"
+)
 
 // EmailTemplate is the HTML template for the email
 const EmailTemplate = `
@@ -19,37 +19,11 @@ const EmailTemplate = `
   <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1" name="viewport">
-    <meta name="x-apple-disable-message-reformatting">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta content="telephone=no" name="format-detection">
-    <title></title>
-    <!--[if (mso 16)]>
-    <style type="text/css">
-    a {text-decoration: none;}
-    </style>
-    <![endif]-->
-    <!--[if gte mso 9]><style>sup { font-size: 100% !important; }</style><![endif]-->
-    <!--[if gte mso 9]>
-<noscript>
-         <xml>
-           <o:OfficeDocumentSettings>
-           <o:AllowPNG></o:AllowPNG>
-           <o:PixelsPerInch>96</o:PixelsPerInch>
-           </o:OfficeDocumentSettings>
-         </xml>
-      </noscript>
-<![endif]-->
-    <!--[if !mso]><!-- -->
+    <title>Secret Santa Message</title>
     <link href="https://fonts.googleapis.com/css2?family=Changa:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
-    <!--<![endif]-->
   </head>
   <body class="body">
     <div dir="ltr" class="es-wrapper-color">
-      <!--[if gte mso 9]>
-			<v:background xmlns:v="urn:schemas-microsoft-com:vml" fill="t">
-				<v:fill type="tile" color="#213049"></v:fill>
-			</v:background>
-		<![endif]-->
       <table width="100%" cellspacing="0" cellpadding="0" class="es-wrapper">
         <tbody>
           <tr>
@@ -57,86 +31,15 @@ const EmailTemplate = `
               <table cellspacing="0" cellpadding="0" align="center" class="es-content esd-header-popover">
                 <tbody>
                   <tr>
-                    <td align="center" background="https://tlr.stripocdn.email/content/guids/CABINET_e585f666dce8a1411b56958e42b81148bc5b3de704825fafe83d86a62a9e4334/images/bg.png" class="esd-stripe" style="background-image:url(https://tlr.stripocdn.email/content/guids/CABINET_e585f666dce8a1411b56958e42b81148bc5b3de704825fafe83d86a62a9e4334/images/bg.png);background-repeat:no-repeat;background-position:center top">
+                    <td align="center">
                       <table width="600" cellspacing="0" cellpadding="0" bgcolor="#f4ece7" align="center" class="es-content-body" style="background-color:#f4ece7">
                         <tbody>
                           <tr>
-                            <td align="left" bgcolor="#1c283d" class="esd-structure es-p20" style="background-color:#1c283d">
-                              <table cellspacing="0" cellpadding="0" width="100%">
-                                <tbody>
-                                  <tr>
-                                    <td width="560" align="left" class="esd-container-frame">
-                                      <table width="100%" cellspacing="0" cellpadding="0">
-                                        <tbody>
-                                          <tr>
-                                            <td align="center" class="esd-block-image" style="font-size:0px">
-                                              <a target="_blank">
-                                                <img src="https://tlr.stripocdn.email/content/guids/CABINET_e585f666dce8a1411b56958e42b81148bc5b3de704825fafe83d86a62a9e4334/images/xmas.png" alt="" width="130" style="display:block">
-                                              </a>
-                                            </td>
-                                          </tr>
-                                        </tbody>
-                                      </table>
-                                    </td>
-                                  </tr>
-                                </tbody>
-                              </table>
-                            </td>
-                          </tr>
-                          <tr>
-                            <td align="left" class="esd-structure">
-                              <table width="100%" cellspacing="0" cellpadding="0">
-                                <tbody>
-                                  <tr>
-                                    <td width="600" valign="top" align="center" class="esd-container-frame">
-                                      <table width="100%" cellspacing="0" cellpadding="0">
-                                        <tbody>
-                                          <tr>
-                                            <td align="center" class="esd-block-image" style="font-size:0px">
-                                              <a target="_blank">
-                                                <img src="https://tlr.stripocdn.email/content/guids/CABINET_e585f666dce8a1411b56958e42b81148bc5b3de704825fafe83d86a62a9e4334/images/bannerchr1.gif" alt="" width="600" class="adapt-img" style="display:block">
-                                              </a>
-                                            </td>
-                                          </tr>
-                                        </tbody>
-                                      </table>
-                                    </td>
-                                  </tr>
-                                </tbody>
-                              </table>
-                            </td>
-                          </tr>
-                          <tr>
-                            <td align="left" bgcolor="#f4ece7" class="esd-structure es-p30t es-p30b es-p20r es-p20l" style="background-color:#f4ece7">
-                              <table cellpadding="0" cellspacing="0" width="100%">
-                                <tbody>
-                                  <tr>
-                                    <td width="560" align="center" valign="top" class="esd-container-frame">
-                                      <table cellpadding="0" cellspacing="0" width="100%">
-                                        <tbody>
-                                          <tr>
-                                            <td align="center" class="esd-block-text">
-                                              <h1 class="es-m-txt-c" style="color:#008c7c;line-height:110%">
-                                                Psst... your Secret Santa has a little
-                                              </h1>
-                                              <h1 class="es-m-txt-c" style="color:#008c7c;line-height:110%">
-                                                message for you!
-                                              </h1>
-                                            </td>
-                                          </tr>
-                                          <tr>
-                                            <td align="center" class="esd-block-text es-p20t es-p20b">
-                                              <p>
-                                                <h2>{{.Message}}</h2>
-                                              </p>
-                                            </td>
-                                          </tr>
-                                        </tbody>
-                                      </table>
-                                    </td>
-                                  </tr>
-                                </tbody>
-                              </table>
+                            <td align="center" class="esd-block-text">
+                              <h1 style="color:#008c7c;line-height:110%">
+                                Psst... your Secret Santa has a little message for you!
+                              </h1>
+                              <h2>{{.Message}}</h2>
                             </td>
                           </tr>
                         </tbody>
@@ -152,7 +55,6 @@ const EmailTemplate = `
     </div>
   </body>
 </html>
-
 `
 
 func SecretMessageHandler(w http.ResponseWriter, r *http.Request) {
@@ -161,8 +63,9 @@ func SecretMessageHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	email := os.Getenv("EMAIL_ADDR")
-	password := os.Getenv("EMAIL_PWD")
+	// We no longer strictly need EMAIL_PWD for the Gmail API via Service Account,
+	// but we still typically check if EMAIL_ADDR is set for context.
+	// email := os.Getenv("EMAIL_ADDR") 
 
 	to := r.FormValue("to")
 	message := r.FormValue("message")
@@ -181,32 +84,55 @@ func SecretMessageHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Compose the email with headers
-	body := fmt.Sprintf("Subject: 🎅Secret message from the Secret Santa 🎅\nMIME-Version: 1.0\nContent-Type: text/html; charset=\"UTF-8\"\n\n%s", bodyBuffer.String())
-
-	from := "Secret Santa <secretsanta@qburst.com>"
-	// Send the email
-	err = sendEmail(email, password, from, to, body)
+	// Send the email using Gmail API
+	err = sendEmailWithGmailAPI(to, bodyBuffer.String())
 	if err != nil {
-		http.Error(w, "Failed to send email", http.StatusInternalServerError)
+		log.Printf("Failed to send email: %v", err)
+		http.Error(w, "Failed to send email: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
+	
 	http.Redirect(w, r, "/", http.StatusSeeOther)
-	return
 }
 
-// Function to send email using SMTP
-func sendEmail(email string, password string, from string, to string, body string) error {
-	auth := smtp.PlainAuth("", email, password, smtpServer)
+// Function to send email using Gmail API (Service Account)
+func sendEmailWithGmailAPI(to string, body string) error {
+	ctx := context.Background()
 
-	// Format the email message
-	msg := []byte(fmt.Sprintf("From: %s\nTo: %s\n%s", from, to, body))
-
-	// Send the email
-	err := smtp.SendMail(smtpServer+":"+smtpPort, auth, email, []string{to}, msg)
+	// 1. Create the Gmail Service using the existing credentials.json
+	srv, err := gmail.NewService(ctx, option.WithCredentialsFile("credentials.json"))
 	if err != nil {
-		log.Println("Error sending email:", err)
-		return err
+		return fmt.Errorf("unable to retrieve Gmail client: %v", err)
 	}
+
+	// 2. Construct the email message
+	// Note: The "From" address is determined by the authenticated Service Account
+	// unless Domain-Wide Delegation is configured.
+	header := make(map[string]string)
+	header["To"] = to
+	header["Subject"] = "🎅Secret message from the Secret Santa 🎅"
+	header["MIME-Version"] = "1.0"
+	header["Content-Type"] = "text/html; charset=\"UTF-8\""
+
+	var msgString string
+	for k, v := range header {
+		msgString += fmt.Sprintf("%s: %s\r\n", k, v)
+	}
+	msgString += "\r\n" + body
+
+	// 3. Encode the message to base64url (required by Gmail API)
+	rawMessage := base64.URLEncoding.EncodeToString([]byte(msgString))
+
+	gMessage := &gmail.Message{
+		Raw: rawMessage,
+	}
+
+	// 4. Send the email
+	// "me" is a special value indicating the authenticated user (the service account)
+	_, err = srv.Users.Messages.Send("me", gMessage).Do()
+	if err != nil {
+		return fmt.Errorf("unable to send message: %v", err)
+	}
+
 	return nil
 }
